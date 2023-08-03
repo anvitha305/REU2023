cb484b376db94367593a68886c2e27cba059c684
Anvitha Ramachandran <44482134+anvitha305@users.noreply.github.com>
2023-08-03 11:51:57 -0400

    MNIST updated test script for local machine with axis plots on confusion matrix.

--- localtests/mnist.py
+++ localtests/mnist.py
@@ -2,32 +2,47 @@ import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten
 import keras
-
-# modules for plotting in this notebook
+import seaborn as sns
+# modules for plotting
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from pylab import savefig
 
 # load MNIST data for the training and test images and labels
 # [need them to evaluate the model]
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+# format the data to run the tests:
+x_test = x_test.reshape(10000, 28*28)
+x_test = x_test.astype("float32")
+
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
 # import the MNIST model to evaluate it on a local machine
 model = keras.models.load_model("mnist.keras")
 
 # get loss and accuracy statistics
-loss, acc = new_model.evaluate(x_test, y_test, verbose=2)
+loss, acc = model.evaluate(x_test, y_test, verbose=2)
 
 # predict values to build a confusion matrix
 y_pred = model.predict(x_test)
 
 # predictions and ground-truth to one-hot vectors
-y_pred_classes = np.argmax(Y_pred,axis = 1)
+y_pred_classes = np.argmax(y_pred,axis = 1)
 y_true = np.argmax(y_test,axis = 1)
+
+
 
 # compute the confusion matrix
 confusion_mtx = tf.math.confusion_matrix(y_true, y_pred_classes) 
 
 # plot confusion matrix
 plt.figure(figsize=(10, 8))
-sns.heatmap(confusion_mtx, annot=True, fmt='g')
+mn = sns.heatmap(confusion_mtx, annot=True, fmt='g')
+plt.xlabel("True Numbers")
+plt.ylabel("Predicted Numbers")
+plt.title("MNIST Dataset Number Prediction Confusion Matrix")
+plt.savefig('mnist_local.png', dpi=400)
+
