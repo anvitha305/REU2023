7f39064f46c0bad1a9edb1d32469d2f2d805b6c3
anvitha305 <anvitha2k20@gmail.com>
2023-08-07 00:11:22 -0400

    final solar.py test for local machine.

--- localtests/solar.py
+++ localtests/solar.py
@@ -2,12 +2,8 @@ import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten
 import keras
-import seaborn as sns
 # modules for plotting
-import numpy as np
-import matplotlib.pyplot as plt
 import random
-from pylab import savefig
 # getting dataset from Roboflow
 from roboflow import Roboflow
 import os
@@ -19,8 +15,12 @@ if not os.path.exists("solar-panel-infrared-images-5/"):
     rf = Roboflow(str(key))
     project = rf.workspace("solarpanelimages").project("solar-panel-infrared-images")
     dataset = project.version(5).download("yolov5")
+if not os.path.exists("yolov5/"):
+    os.system("git clone https://github.com/ultralytics/yolov5")
+    os.system("pip3 install -qr yolov5/requirements.txt")
+    os.system("cd ..")
 test_set_loc = "solar-panel-infrared-images-5/test/images/"
 random_test_image = random.choice(os.listdir(test_set_loc))
 print(random_test_image)
-os.system("python3 detect.py --weights solar_model --img 416 --conf 0.1 --source " + test_set_loc)
-
+os.system("python3 yolov5/detect.py --weights solar.pt --img 416 --conf 0.1 --source " + test_set_loc+random_test_image)
+os.system("python3 yolov5/val.py --weights solar.pt --img 416 --conf 0.1 --data solar-panel-infrared-images-5/data.yaml")
