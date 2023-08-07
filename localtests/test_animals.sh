9d0d410d4e146fda9e36e2e36f4c9210d9b5f242
anvitha305 <anvitha2k20@gmail.com>
2023-08-07 15:38:59 -0400

    fixed

--- localtests/test_animals.sh
+++ localtests/test_animals.sh
@@ -1,10 +1,13 @@
 sleep 30
 rm -r "yolov5/runs/detect/"
 rm -r "yolov5/runs/val/"
-for (( c=1; c<=1; c++ ))
+for (( c=1; c<=10; c++ ))
 do
 	python3 animals.py
 	sleep 30
 done
+mkdir "animals"
+mkdir "animals/detect"
+mkdir "animals/val"
 cp -r "yolov5/runs/detect/" "animals/detect"
 cp -r "yolov5/runs/val/" "animals/val"
--- localtests/test_solar.sh
+++ localtests/test_solar.sh
@@ -1,10 +1,13 @@
-#sleep 30
+sleep 30
 rm -r "yolov5/runs/detect/"
 rm -r "yolov5/runs/val/"
-for (( c=1; c<=1; c++ ))
+for (( c=1; c<=10; c++ ))
 do
 	python3 solar.py
-	#sleep 30
+	sleep 30
 done
+mkdir "solar"
+mkdir "solar/detect"
+mkdir "solar/val"
 cp -r "yolov5/runs/detect/" "solar/detect"
 cp -r "yolov5/runs/val/" "solar/val"
