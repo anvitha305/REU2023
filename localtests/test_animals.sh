1f32fbfa547559abe1ff241fbd07a2d384970fda
anvitha305 <anvitha2k20@gmail.com>
2023-08-07 13:57:25 -0400

    fixed...

--- localtests/test_animals.sh
+++ localtests/test_animals.sh
@@ -1,10 +1,10 @@
-#sleep 30
+sleep 30
 rm -r "yolov5/runs/detect/"
 rm -r "yolov5/runs/val/"
 for (( c=1; c<=1; c++ ))
 do
 	python3 animals.py
-	#sleep 30
+	sleep 30
 done
 cp -r "yolov5/runs/detect/" "animals/detect"
 cp -r "yolov5/runs/val/" "animals/val"
